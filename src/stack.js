9a11a118350499fe18bb6e2f48e0b564ea385b79
Pavel Pleshkov <pleshkov.p@gmail.com>
2022-05-03 02:55:05 +0300

    feat: start solving, some in stack.js

--- src/stack.js
+++ src/stack.js
@@ -13,21 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.body = [];
+    this.length = this.body.length;
+  }
+  push(element) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.body.push(element);
+    console.log(this.body);
+
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    console.log(this.body);
+
+    console.log(this.body[this.body.length - 1]);
+    this.body.pop()
+    
+    return this.body[this.body.length - 1]
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    console.log(this.body[this.body.length - 1]);
+
+    return this.body[this.body.length - 1];
   }
 }
+
+const stack = new Stack();
+stack.push(1); // adds the element to the stack
+stack.peek(); // returns the peek, but doesn't delete it, returns 1
+stack.pop(); // returns the top element from stack and deletes it, returns 1
+stack.pop(); // undefined
 
 module.exports = {
   Stack
